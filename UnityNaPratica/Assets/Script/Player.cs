62fbf4b0303152f4da33b6c18c412358dc334eee
hulken1 <sergio_dognini@yahoo.com.br>
2019-12-10 17:24:53 -0300

    Primeiro DIA

---
+++ UnityNaPratica/Assets/Script/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed; 
+    public int jumpForce;
+    public int health;
+    public Transform groundCheck;
+    
+    private bool invunerable = false;
+    private bool grounded = false;
+    private bool jumping = false;
+    private bool facingRight = true;
+
+    private   SpriteRenderer sprite;
+    private RigidBody2D rb2d;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
