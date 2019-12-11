b7cedc4d8df0a78b1236b2dc54b7652137c2d41c
hulken1 <sergio_dognini@yahoo.com.br>
2019-12-11 17:24:55 -0300

    dia 2

--- UnityNaPratica/Assets/Script/Player.cs
+++ UnityNaPratica/Assets/Script/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,19 +16,120 @@ public class Player : MonoBehaviour
     private bool jumping = false;
     private bool facingRight = true;
 
-    private   SpriteRenderer sprite;
-    private RigidBody2D rb2d;
+    private SpriteRenderer sprite;
+    private Rigidbody2D rb2d;
     private Animator anim;
+
+    public float attackRate;
+    public Transform spawnAttack;
+    public GameObject attackPrefab;
+    private float nextAttack = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         sprite = GetComponent<SpriteRenderer> ();
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Flip()
+    {
+        //Validando se está virado para direita ou esquerda
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(-transform.localScale.x,transform.localScale.y,transform.localScale.z); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Verificando se o personagem esta tocando o chão
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            jumping = true;
+        }
+
+        SetAnimations();
+
+        //animacao do attack
+        if (Input.GetButton("Fire1") && (Time.time > nextAttack))
+        {
+            Attack();
+        }
+
+    }
+    void FixedUpdate()
+    {
+        //Criando o movimento a partir do Speed
+        float move = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(move * speed, rb2d.velocity.y);
+
+        if ((move < 0f && facingRight) || (move > 0f & !facingRight))
+        {
+            Flip();
+        }
+
+        if (jumping)
+        {
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+            jumping = false;
+        }
+    }
+    //Criando attack
+    void Attack()
+    {
+        anim.SetTrigger("Punch");
+
+        nextAttack = Time.time + attackRate;
+
+        GameObject cloneAttack = Instantiate(attackPrefab, spawnAttack.position, spawnAttack.rotation);
+
+        if (!facingRight)
+        {
+            cloneAttack.transform.eulerAngles = new Vector3(180, 0, 180);
+        }
+
+    }
+    void SetAnimations()
+    {
+        anim.SetFloat("VelY", rb2d.velocity.y);
+        anim.SetBool("JumpFall", !grounded);
+        anim.SetBool("Walk", rb2d.velocity.x != 0f && grounded);
+
+
+    }
+
+    IEnumerator DamageEffect()
+    {
+        //criar efeito de camera
+
+        for (float i = 0f; i < 1f; i+= 0.1f)
+        {
+            sprite.enabled = false;
+            yield return new WaitForSeconds(0.1f);
+            sprite.enabled = true;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        invunerable = false;
+    }
+
+    public void DamagePlayer()
+    {
+        if (!invunerable)
+        {
+            invunerable = true;
+            health--;
+            StartCoroutine(DamageEffect());
+
+            if (health < 1)
+            {
+
+            }
+        }
         
     }
+
 }
---
+++ UnityNaPratica/Assets/Script/Snake.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    public float speed;
+    public int health;
+    public Transform wallCheck;
+
+    private bool facingRight = true;
+
+    private SpriteRenderer sprite;
+    private Rigidbody2D rb2d;
+
+    private bool tocheddWall = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {      
+        tocheddWall = Physics2D.Linecast(transform.position, wallCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+        if (tocheddWall)
+        {
+            Flip();
+        }
+
+    }
+    void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+    }
+    //Logica para mudar de lado e andar 
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y,transform.localScale.z);
+        speed *= -1;
+    }
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.CompareTag("Attack"))
+        {
+            DamageEnemy();
+        }
+    }
+
+    IEnumerator DamageEffect()
+    {
+        float actualSpeed = speed;
+        speed = speed * -1;
+        sprite.color = Color.red;
+        rb2d.AddForce(new Vector2(0f, 200f));
+        yield return new WaitForSeconds(0.1f);
+        speed = actualSpeed;
+        sprite.color = Color.white;
+        
+    }
+    void DamageEnemy()
+    {
+        health--;
+        StartCoroutine(DamageEffect());
+
+        if(health < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
